055d5c963e316cb71bf72aade568a673d1620b46
hanjihoon03 <163777923+hanjihoon03@users.noreply.github.com>
2024-05-02 22:33:07 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,53 +6,80 @@ cd /home/ubuntu/app
 # 환경변수 DOCKER_APP_NAME을 spring으로 설정
 DOCKER_APP_NAME=spring
 
-
-# 실행중인 blue가 있는지 확인
-# 프로젝트의 실행 중인 컨테이너를 확인하고, 해당 컨테이너가 실행 중인지 여부를 EXIST_BLUE 변수에 저장
+# 실행중인 blue와 green 확인
 EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+EXIST_GREEN=$(sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml ps | grep Up)
 
 # 배포 시작한 날짜와 시간을 기록
 echo "배포 시작일자 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
 
-# green이 실행중이면 blue up
-# EXIST_BLUE 변수가 비어있는지 확인
-if [ -z "$EXIST_BLUE" ]; then
+# 둘 다 실행 중이지 않으면 blue를 시작하고 그렇지 않으면 green을 시작
+if [ -z "$EXIST_BLUE" ] && [ -z "$EXIST_GREEN" ]; then
 
-  # 로그 파일(/home/ec2-user/deploy.log)에 "blue up - blue 배포 : port:8081"이라는 내용을 추가
+  # 로그 파일에 "blue 배포 시작"이라는 내용을 추가
   echo "blue 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
 
-	# docker-compose.blue.yml 파일을 사용하여 spring-blue 프로젝트의 컨테이너를 빌드하고 실행
-	sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
+  # docker-compose.blue.yml 파일을 사용하여 spring-blue 프로젝트의 컨테이너를 빌드하고 실행
+  sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
 
   # 30초 동안 대기
   sleep 30
 
-  # /home/ec2-user/deploy.log: 로그 파일에 "green 중단 시작"이라는 내용을 추가
-  echo "green 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
-
-  # docker-compose.green.yml 파일을 사용하여 spring-green 프로젝트의 컨테이너를 중지
-  sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
-
-   # 사용하지 않는 이미지 삭제
+  # 사용하지 않는 이미지 삭제
   sudo docker image prune -af
 
-  echo "green 중단 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
+  # 로그 파일에 "blue 배포 완료"이라는 내용을 추가
+  echo "blue 배포 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
 
-# blue가 실행중이면 green up
-else
-	echo "green 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
-	sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
+# green이 실행 중이지 않으면 blue를 시작하고 green이 실행 중이면 blue를 중지하고 green을 시작
+elif [ -z "$EXIST_GREEN" ]; then
 
+  # 로그 파일에 "blue 배포 시작"이라는 내용을 추가
+  echo "blue 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
+
+  # docker-compose.blue.yml 파일을 사용하여 spring-blue 프로젝트의 컨테이너를 빌드하고 실행
+  sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
+
+  # 30초 동안 대기
   sleep 30
 
-  echo "blue 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
-  sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+  # 사용하지 않는 이미지 삭제
   sudo docker image prune -af
 
+  # 로그 파일에 "blue 배포 완료"이라는 내용을 추가
+  echo "blue 배포 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
+
+# 그렇지 않으면 blue를 중지하고 green을 시작
+else
+  # 로그 파일에 "blue 중단 시작"이라는 내용을 추가
+  echo "blue 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
+
+  # docker-compose.blue.yml 파일을 사용하여 spring-blue 프로젝트의 컨테이너를 중지
+  sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+
+  # 로그 파일에 "blue 중단 완료"이라는 내용을 추가
   echo "blue 중단 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
 
-fi
-  echo "배포 종료  : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
+  # 로그 파일에 "green 배포 시작"이라는 내용을 추가
+  echo "green 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
 
-  echo "===================== 배포 완료 =====================" >> /home/ubuntu/deploy.log
-  echo >> /home/ubuntu/deploy.log
+  # docker-compose.green.yml 파일을 사용하여 spring-green 프로젝트의 컨테이너를 빌드하고 실행
+  sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
+
+  # 30초 동안 대기
+  sleep 30
+
+  # 사용하지 않는 이미지 삭제
+  sudo docker image prune -af
+
+  # 로그 파일에 "green 배포 완료"이라는 내용을 추가
+  echo "green 배포 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
+
+fi
+
+# 배포 종료 날짜와 시간을 로그 파일에 추가
+echo "배포 종료일자 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
+
+# 배포 완료 메시지를 로그 파일에 추가
+echo "===================== 배포 완료 =====================" >> /home/ubuntu/deploy.log
+echo >> /home/ubuntu/deploy.log
